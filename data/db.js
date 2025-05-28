dd6459e605c545b57b70caf3ecb69cc83530724b
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-28 16:32:20 +0200

    [ADD] Created database configuration file and hidden password

---
+++ data/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+//importo mysql2
+const mysql = require("mysql2");
+
+//creo una variabile per la connessione al database
+//è un oggeto che ha bisogno di queste proprietà
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+//utilizzo l avariabile per instaurare una connesione al database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL")
+});
+
+module.exports = connection
