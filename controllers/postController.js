f7b19e4c95a8f8a4ebd0423b1e0880fb30d3d346
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-05-28 16:56:26 +0200

    [UPD] Database connected

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,6 +1,17 @@
+//recupero la variabile di connesione al database
+const connection = require('../data/db')
 
 //metodo index
 const index = (req, res) => {
+    //preparo la query per recuperare i post dal database
+    const sql = "SELECT * FROM  posts";
+
+    //eseguo la query con il metodo connection
+    connection.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: "Database query failed" })
+        }
+    })
     res.send("Elenco post")
 }
 
